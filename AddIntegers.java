0dd5876690b3bd710b387bb864a12ac5c94097f7
meghana202006 <meghanarodd20@gmail.com>
2025-03-04 12:06:37 +0530

    Add files via upload

---
+++ AddIntegers.java
@@ -0,0 +1,14 @@
+public class AddIntegers {
+
+    public static void main(String[] args) {
+        // Initializing two integers
+        int num1 = 5;
+        int num2 = 10;
+
+        // Adding the integers
+        int sum = num1 + num2;
+
+        // Printing the result
+        System.out.println("The sum of " + num1 + " and " + num2 + " is: " + sum);
+    }
+}
