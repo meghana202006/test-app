fe9855351f93dd51d3f83379efdab45a0080191c
meghana202006 <meghanarodd20@gmail.com>
2025-04-21 02:33:43 +0530

    Update2 SumOfElements.java

--- SumOfElements.java
+++ SumOfElements.java
@@ -6,7 +6,7 @@ public class SumOfElements
 		int sum = 0;
 		for(int i = 0; i < args.length; i++)
 		{
-			sum = sum + Integer.parseInt(args[i]);
+			sum = sum + Integer.parseInt(args[i]
 		}
 		System.out.println("Sum of given elements is: "+sum);
 	
