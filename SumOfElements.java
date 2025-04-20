a3946d725fe92090f4266d409e1388a5d8c09c90
meghana202006 <meghanarodd20@gmail.com>
2025-04-21 01:08:57 +0530

    Add files via upload

---
+++ SumOfElements.java
@@ -0,0 +1,14 @@
+public class SumOfElements
+{
+
+	public static void main(String args[])
+	{
+		int sum = 0;
+		for(int i = 0; i < args.length; i++)
+		{
+			sum = sum + Integer.parseInt(args[i]);
+		}
+		System.out.println("Sum of given elements is: "+sum);
+	
+	}
+}
